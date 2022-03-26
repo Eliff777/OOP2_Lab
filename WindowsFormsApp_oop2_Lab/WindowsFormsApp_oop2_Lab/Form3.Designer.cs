c38a745eb45c51432de316375962683d0e0d36a0
Elif can <elifcan0777@gmail.com>
2022-03-26 17:12:46 +0300

    form 3 eklendi. dosyaya yazma ve dosyadan okuma işlemleri tamamlandı

--- WindowsFormsApp_oop2_Lab/WindowsFormsApp_oop2_Lab/Form3.Designer.cs
+++ WindowsFormsApp_oop2_Lab/WindowsFormsApp_oop2_Lab/Form3.Designer.cs
@@ -37,36 +37,40 @@
             this.Normal = new System.Windows.Forms.RadioButton();
             this.Hard = new System.Windows.Forms.RadioButton();
             this.Custom = new System.Windows.Forms.RadioButton();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.input1 = new System.Windows.Forms.TextBox();
+            this.input2 = new System.Windows.Forms.TextBox();
             this.save = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // Shape
             // 
             this.Shape.AutoSize = true;
-            this.Shape.Location = new System.Drawing.Point(54, 21);
+            this.Shape.Font = new System.Drawing.Font("Lucida Bright", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Shape.Location = new System.Drawing.Point(53, 39);
             this.Shape.Name = "Shape";
-            this.Shape.Size = new System.Drawing.Size(49, 17);
+            this.Shape.Size = new System.Drawing.Size(58, 20);
             this.Shape.TabIndex = 0;
             this.Shape.Text = "Shape";
             // 
             // Square
             // 
             this.Square.AutoSize = true;
-            this.Square.Location = new System.Drawing.Point(57, 59);
+            this.Square.Font = new System.Drawing.Font("Lucida Bright", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Square.Location = new System.Drawing.Point(56, 72);
             this.Square.Name = "Square";
-            this.Square.Size = new System.Drawing.Size(76, 21);
+            this.Square.Size = new System.Drawing.Size(88, 24);
             this.Square.TabIndex = 1;
             this.Square.Text = "Square";
             this.Square.UseVisualStyleBackColor = true;
+            this.Square.CheckedChanged += new System.EventHandler(this.Square_CheckedChanged);
             // 
             // Triangle
             // 
             this.Triangle.AutoSize = true;
-            this.Triangle.Location = new System.Drawing.Point(57, 87);
+            this.Triangle.Font = new System.Drawing.Font("Lucida Bright", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Triangle.Location = new System.Drawing.Point(57, 102);
             this.Triangle.Name = "Triangle";
-            this.Triangle.Size = new System.Drawing.Size(82, 21);
+            this.Triangle.Size = new System.Drawing.Size(101, 24);
             this.Triangle.TabIndex = 2;
             this.Triangle.Text = "Triangle";
             this.Triangle.UseVisualStyleBackColor = true;
@@ -74,9 +78,10 @@
             // Round
             // 
             this.Round.AutoSize = true;
-            this.Round.Location = new System.Drawing.Point(57, 115);
+            this.Round.Font = new System.Drawing.Font("Lucida Bright", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Round.Location = new System.Drawing.Point(57, 132);
             this.Round.Name = "Round";
-            this.Round.Size = new System.Drawing.Size(72, 21);
+            this.Round.Size = new System.Drawing.Size(87, 24);
             this.Round.TabIndex = 3;
             this.Round.Text = "Round";
             this.Round.UseVisualStyleBackColor = true;
@@ -84,76 +89,86 @@
             // difficulty
             // 
             this.difficulty.AutoSize = true;
-            this.difficulty.Location = new System.Drawing.Point(319, 21);
+            this.difficulty.Font = new System.Drawing.Font("Lucida Bright", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.difficulty.Location = new System.Drawing.Point(240, 39);
             this.difficulty.Name = "difficulty";
-            this.difficulty.Size = new System.Drawing.Size(61, 17);
+            this.difficulty.Size = new System.Drawing.Size(91, 20);
             this.difficulty.TabIndex = 4;
             this.difficulty.Text = "Difficulty";
             // 
             // Easy
             // 
             this.Easy.AutoSize = true;
-            this.Easy.Location = new System.Drawing.Point(322, 59);
+            this.Easy.Font = new System.Drawing.Font("Lucida Bright", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Easy.Location = new System.Drawing.Point(248, 71);
             this.Easy.Name = "Easy";
-            this.Easy.Size = new System.Drawing.Size(60, 21);
+            this.Easy.Size = new System.Drawing.Size(68, 24);
             this.Easy.TabIndex = 5;
             this.Easy.TabStop = true;
             this.Easy.Text = "Easy";
             this.Easy.UseVisualStyleBackColor = true;
+            this.Easy.CheckedChanged += new System.EventHandler(this.Easy_CheckedChanged);
             // 
             // Normal
             // 
             this.Normal.AutoSize = true;
-            this.Normal.Location = new System.Drawing.Point(322, 87);
+            this.Normal.Font = new System.Drawing.Font("Lucida Bright", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Normal.Location = new System.Drawing.Point(248, 101);
             this.Normal.Name = "Normal";
-            this.Normal.Size = new System.Drawing.Size(74, 21);
+            this.Normal.Size = new System.Drawing.Size(95, 24);
             this.Normal.TabIndex = 6;
             this.Normal.TabStop = true;
             this.Normal.Text = "Normal";
             this.Normal.UseVisualStyleBackColor = true;
+            this.Normal.CheckedChanged += new System.EventHandler(this.Normal_CheckedChanged);
             // 
             // Hard
             // 
             this.Hard.AutoSize = true;
-            this.Hard.Location = new System.Drawing.Point(322, 115);
+            this.Hard.Font = new System.Drawing.Font("Lucida Bright", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Hard.Location = new System.Drawing.Point(248, 131);
             this.Hard.Name = "Hard";
-            this.Hard.Size = new System.Drawing.Size(60, 21);
+            this.Hard.Size = new System.Drawing.Size(72, 24);
             this.Hard.TabIndex = 7;
             this.Hard.TabStop = true;
             this.Hard.Text = "Hard";
             this.Hard.UseVisualStyleBackColor = true;
+            this.Hard.CheckedChanged += new System.EventHandler(this.Hard_CheckedChanged);
             // 
             // Custom
             // 
             this.Custom.AutoSize = true;
-            this.Custom.Location = new System.Drawing.Point(322, 143);
+            this.Custom.Font = new System.Drawing.Font("Lucida Bright", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Custom.Location = new System.Drawing.Point(248, 161);
             this.Custom.Name = "Custom";
-            this.Custom.Size = new System.Drawing.Size(76, 21);
+            this.Custom.Size = new System.Drawing.Size(98, 24);
             this.Custom.TabIndex = 8;
             this.Custom.TabStop = true;
             this.Custom.Text = "Custom";
             this.Custom.UseVisualStyleBackColor = true;
+            this.Custom.CheckedChanged += new System.EventHandler(this.Custom_CheckedChanged);
             // 
-            // textBox1
+            // input1
             // 
-            this.textBox1.Location = new System.Drawing.Point(320, 180);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(76, 22);
-            this.textBox1.TabIndex = 9;
+            this.input1.Location = new System.Drawing.Point(244, 204);
+            this.input1.Name = "input1";
+            this.input1.Size = new System.Drawing.Size(76, 22);
+            this.input1.TabIndex = 9;
             // 
-            // textBox2
+            // input2
             // 
-            this.textBox2.Location = new System.Drawing.Point(402, 180);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(74, 22);
-            this.textBox2.TabIndex = 10;
+            this.input2.Location = new System.Drawing.Point(326, 204);
+            this.input2.Name = "input2";
+            this.input2.Size = new System.Drawing.Size(74, 22);
+            this.input2.TabIndex = 10;
             // 
             // save
             // 
             this.save.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.save.Location = new System.Drawing.Point(483, 226);
+            this.save.Font = new System.Drawing.Font("Lucida Bright", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.save.Location = new System.Drawing.Point(455, 186);
             this.save.Name = "save";
-            this.save.Size = new System.Drawing.Size(75, 31);
+            this.save.Size = new System.Drawing.Size(86, 40);
             this.save.TabIndex = 11;
             this.save.Text = "Save";
             this.save.UseVisualStyleBackColor = false;
@@ -165,8 +180,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.save);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.input2);
+            this.Controls.Add(this.input1);
             this.Controls.Add(this.Custom);
             this.Controls.Add(this.Hard);
             this.Controls.Add(this.Normal);
@@ -178,6 +193,7 @@
             this.Controls.Add(this.Shape);
             this.Name = "Form3";
             this.Text = "Form3";
+            this.Load += new System.EventHandler(this.Form3_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -194,8 +210,8 @@
         private System.Windows.Forms.RadioButton Normal;
         private System.Windows.Forms.RadioButton Hard;
         private System.Windows.Forms.RadioButton Custom;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox input1;
+        private System.Windows.Forms.TextBox input2;
         private System.Windows.Forms.Button save;
     }
 }
--- WindowsFormsApp_oop2_Lab/WindowsFormsApp_oop2_Lab/Form3.cs
+++ WindowsFormsApp_oop2_Lab/WindowsFormsApp_oop2_Lab/Form3.cs
@@ -6,6 +6,8 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+
 using System.Windows.Forms;
 
 namespace WindowsFormsApp_oop2_Lab
@@ -19,7 +21,121 @@ namespace WindowsFormsApp_oop2_Lab
 
         private void save_Click(object sender, EventArgs e)
         {
+            String myfile1 = @"log.txt";
+            File.Delete(myfile1);
+            Form2 form2 = new Form2();
+            this.Hide();
+            form2.Show();
+            using (StreamWriter w = File.AppendText("log.txt"))
+            {
+                if (Square.Checked == true)
+                {
+                    w.WriteLine("Square");
+                }
+                if (Triangle.Checked == true)
+                {
+                    w.WriteLine("Triangle");
+                }
+                if (Round.Checked == true)
+                {
+                    w.WriteLine("Round");
+                }
+                if (Easy.Checked == true)
+                {
+                    w.WriteLine("Easy");
+                }
+                if (Normal.Checked == true)
+                {
+                    w.WriteLine("Normal");
+                }
+                if (Hard.Checked == true)
+                {
+                    w.WriteLine("Hard");
+                }
+                if (Custom.Checked == true)
+                {
+                    w.WriteLine("Custom");
+                    w.WriteLine(input1.Text);
+                    w.WriteLine(input2.Text);
+                }
+                w.Close();
+            }
+        }
 
+        private void Easy_CheckedChanged(object sender, EventArgs e)
+        {
+            input1.Visible = false;
+            input2.Visible = false;
+        }
+
+        private void Normal_CheckedChanged(object sender, EventArgs e)
+        {
+            input1.Visible = false;
+            input2.Visible = false;
+        }
+
+        private void Hard_CheckedChanged(object sender, EventArgs e)
+        {
+            input1.Visible = false;
+            input2.Visible = false;
+        }
+
+        private void Custom_CheckedChanged(object sender, EventArgs e)
+        {
+            input1.Visible = true;
+            input2.Visible = true;
+        }
+
+        private void Square_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            List<string> satirlarList = new List<string>(); //Satırların list'e aktarıp daha sonra kullanmamız için yeni bir değişken tanımlıyoruz. 
+            try {
+                using (StreamReader sr = new StreamReader("log.txt")) //StreamReader fonksiyonu ile okunacak dosyamızı açtırıyoruz.
+                {
+                    string satir; //burada okuduğunuz her satırı atamamız için gerekli değişkeni tanımlıyoruz.
+                    while ((satir = sr.ReadLine()) != null) //Döngü kurup eğer satır boş değilse, satirlarList List'ine ekleme yapıyoruz.
+                    {
+                        if (satir == "Square")
+                        {
+                            Square.Checked = true;
+                        }
+                        if (satir == "Triangle")
+                        {
+                            Triangle.Checked = true;
+                        }
+                        if (satir == "Round")
+                        {
+                            Round.Checked = true;
+                        }
+                        if (satir == "Easy")
+                        {
+                            Easy.Checked = true;
+                        }
+                        if (satir == "Normal")
+                        {
+                            Normal.Checked = true;
+                        }
+                        if (satir == "Hard")
+                        {
+                            Hard.Checked = true;
+                        }
+                        if (satir == "Custom")
+                        {
+                            Custom.Checked = true;
+                            input1.Text = sr.ReadLine();
+                            input2.Text = sr.ReadLine();
+                            break;
+                        }
+                    }
+                }
+            }
+            catch { }
+            
         }
     }
 }
